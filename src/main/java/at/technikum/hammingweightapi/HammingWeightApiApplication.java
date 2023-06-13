73816f210ab9285e794f5e50fa35868c70c27e9a
MarcoHudec <marco.hudec3@gmail.com>
2023-06-13 11:29:09 +0200

    Initial commit

---
+++ src/main/java/at/technikum/hammingweightapi/HammingWeightApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.hammingweightapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammingWeightApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HammingWeightApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/hammingweightapi/HammingWeightApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.hammingweightapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammingWeightApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
