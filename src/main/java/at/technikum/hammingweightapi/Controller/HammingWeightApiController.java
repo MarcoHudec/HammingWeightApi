58926625eee9f2aa539f6180132cf7bd61a8aa34
MarcoHudec <marco.hudec3@gmail.com>
2023-06-13 12:19:36 +0200

    created the feature/calculateHamming branch and added the HammingWeightApiController.java in the Controller package

---
+++ src/main/java/at/technikum/hammingweightapi/Controller/HammingWeightApiController.java
@@ -0,0 +1,11 @@
+package at.technikum.hammingweightapi.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HammingWeightApiController {
+
+
+}
